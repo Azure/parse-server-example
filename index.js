5142980668b191f8ac9d8828c1550d3eca19f719
Felix Rieseberg <felix@felixrieseberg.com>
2016-03-17 10:37:14 -0700

    :truck: Add Azure Storage FilesAdapter

--- index.js
+++ index.js
@@ -3,6 +3,22 @@
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
+var AzureStorageAdapter = require('parse-server-azure').FilesAdapter;
+
+var account, container, storageOptions, filesAdapter;
+
+if (process.env.STORAGE_ACCOUNT_NAME) {
+  account = process.env.STORAGE_ACCOUNT_NAME;
+  container = process.env.STORAGE_CONTAINER;
+  storageOptions = {
+    accessKey: process.env.STORAGE_ACCESS_KEY,
+    directAccess: process.env.STORAGE_DIRECT_ACCESS || false // If set to true, files will be served by Azure Blob Storage directly
+  }
+  
+  if (account && container && storageOptions) {
+    filesAdapter = new AzureStorageAdapter(account, container, storageOptions);
+  }
+}
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI
 
@@ -14,7 +30,8 @@ var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '' //Add your master key here. Keep it secret!
+  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  filesAdapter: (filesAdapter) ? filesAdapter : undefined
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
